7956ced3ed895aead4e9e4709edce8e019524801
Thomas <castella.thomas30@gmail.com>
2025-01-28 12:45:41 +0100

    Revert "ajout map test et dessin personnage bon commit cett efois"
    This reverts commit 1f7af30dc33f8778cbac80e738003f1e79f71d10.

--- lib/initSDL.h
+++ lib/initSDL.h
@@ -1,19 +1,13 @@
-#ifndef INIT_SDL_H
-#define INIT_SDL_H
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_image.h>
 
 // Structure qui contient les initialisation de rendu du jeu 
-typedef struct games {
+typedef struct Game {
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* backgroundTexture;
-} game_t;
+} Game;
 
-int initGame(game_t* game); //initialiser SDL
-void cleanUp(game_t* game); //quitter proprement SDL
-
-
-#endif // INIT_SDL_H
+int initGame(Game* game); //initialiser SDL
+void cleanUp(Game* game); //quitter proprement SDL
