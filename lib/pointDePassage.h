bd2920ffb435ae512b7511e65749e93ab35f3eee
Delakom <153620325+Delakom@users.noreply.github.com>
2025-02-28 11:29:38 +0100

    Ajout de la fonction afficherMenuPause dans le jeux

--- lib/pointDePassage.h
+++ lib/pointDePassage.h
@@ -1,6 +1,7 @@
 #ifndef POINT_DE_PASSAGE_H
 #define POINT_DE_PASSAGE_H
 #include "affichage.h"
+#include "menu.h"
 #include "sauv.h"
 
 extern mechas_t mecha[24];
@@ -14,15 +15,15 @@ int copie_mechas(joueur_t *j,mechas_joueur_t *mecha);
 
 int choix_starter(joueur_t *j,pnj_t *vinGazole);
 
-int parler_a_vin_gazole(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p);
+int parler_a_vin_gazole(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,parametre_t *parametres);
 
-int premier_combat_musk(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p);
+int premier_combat_musk(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,parametre_t *parametres);
 
-int retourner_parler_a_vin_gazole(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p);
+int retourner_parler_a_vin_gazole(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,parametre_t *parametres);
 
-int combat_final(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p);
+int combat_final(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,parametre_t *parametres);
 
-int jeu_libre(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p);
+int jeu_libre(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,parametre_t *parametres);
 
 #endif
 
