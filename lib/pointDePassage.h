9e6adcafc004fb0769f8f4cb2e8adc00aa568703
Delakom <153620325+Delakom@users.noreply.github.com>
2025-03-05 16:45:37 +0100

    modification des paramètre pour l'utilisation du fonction afficher_dialogue

--- lib/pointDePassage.h
+++ lib/pointDePassage.h
@@ -9,17 +9,17 @@ int soigner(joueur_t *j);
 
 int copie_mechas(joueur_t *j,mechas_joueur_t *mecha);
 
-int choix_starter(joueur_t *j,pnj_t *vinGazole);
+int choix_starter(game_t *game, joueur_t *j, pnj_t *vinGazole, SDL_Rect *sprite_p, SDL_Rect *pnj_sprite, img_pnj_t * sprite_pnj, img_player_t * sprite_playerH);
 
-int parler_a_vin_gazole(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p, const Uint8 *keys);
+int parler_a_vin_gazole(game_t *game, joueur_t *j, SDL_Rect *sprite_p, SDL_Rect *pnj_sprite, img_pnj_t * sprite_pnj, img_player_t * sprite_playerH, const Uint8 *keys);
 
-int premier_combat_musk(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p, const Uint8 *keys);
+int premier_combat_musk(game_t *game, joueur_t *j, SDL_Rect *sprite_p, SDL_Rect *pnj_sprite, img_pnj_t * sprite_pnj, img_player_t * sprite_playerH, const Uint8 *keys);
 
-int retourner_parler_a_vin_gazole(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,const Uint8 *keys);
+int retourner_parler_a_vin_gazole(game_t *game, joueur_t *j, SDL_Rect *sprite_p, SDL_Rect *pnj_sprite, img_pnj_t * sprite_pnj, img_player_t * sprite_playerH, const Uint8 *keys);
 
-int combat_final(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,const Uint8 *keys);
+int combat_final(game_t *game, joueur_t *j, SDL_Rect *sprite_p, SDL_Rect *pnj_sprite, img_pnj_t * sprite_pnj, img_player_t * sprite_playerH, const Uint8 *keys);
 
-int jeu_libre(game_t *game, img_player_t *sprite_playerH, joueur_t *j,SDL_Rect *sprite_p,const Uint8 *keys);
+int jeu_libre(game_t *game, joueur_t *j, SDL_Rect *sprite_p, SDL_Rect *pnj_sprite, img_pnj_t * sprite_pnj, img_player_t * sprite_playerH, const Uint8 *keys);
 
 #endif
 
