7687f10a44678a29bf5410fc6451648b784e379a
Delakom <153620325+Delakom@users.noreply.github.com>
2025-04-04 21:28:14 +0200

    finalisation de la documentation sur son.h

--- lib/son.h
+++ lib/son.h
@@ -1,3 +1,10 @@
+/**
+ * @file son.h
+ * @brief Gestion de la musique et des effets sonore du jeu.
+ * 
+ * Ce fichier contient les déclarations des fonctions permettant de lancer une musique,
+ * faire des bruitage à partir des paramètres
+ */
 #include "../lib/global.h"
 
 void regler_volume(parametre_t * parametres);
